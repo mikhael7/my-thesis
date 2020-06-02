04323c0a08c875747fc4c4c04a3464d37abeef81
Mikhael <mikhaellion@gmail.com>
2020-06-02 21:51:54 +0700

    paganation-update-notfix

--- kategoriwisata_paganation.php
+++ kategoriwisata_paganation.php
@@ -64,23 +64,8 @@
     <div class="justify-content-center row">
         
         <?php
-            // start paganation
-            if (isset($_GET['pageno'])) {
-                $pageno = $_GET['pageno'];
-            } else {
-                $pageno = 1;
-            }
-            $no_of_records_per_page = 10;
-            $offset = ($pageno-1) * $no_of_records_per_page;
-
-            $total_pages_sql = "SELECT COUNT (*) FROM info";
-            $result = mysqli_query ($connection, $total_pages_sql);
-            $total_rows = "mysqli_fetch_array($result)[0]";
-            $total_pages = ceil($total_rows / $no_of_records_per_page);
-
-            $sql = "SELECT * FROM info LIMIT $offset, $no_of_records_per_page";
-            $res_data = mysqli_query($connection, $sql);
-            if(mysqli_num_rows($res_data)==0) {
+            $infokupu = mysqli_query($connection, "SELECT * FROM info LIMIT 15");
+            if(mysqli_num_rows($infokupu)==0) {
                 echo'
                     <div class="notfound">
                         <img src="./nodata-img/noimage.png">
@@ -88,7 +73,7 @@
                     </div>
                 ';
             }
-            while ($row=mysqli_fetch_assoc($res_data)) {    
+            while ($row=mysqli_fetch_assoc($infokupu)) {    
                 echo'
                     <div class="col-sm-4 col-md-4 col-lg-4 d-flex align-items-stretch card">
                         <img class="card-img-top" src="./admin2/upload/info/'.$row['info_foto'].'" alt="'.$row['info_nama'].'">
@@ -108,17 +93,6 @@
                 ';
             }
         ?>
-
-        <ul class="pagination">
-            <li><a href="?pageno=1">First</a></li>
-            <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-                <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
-            </li>
-            <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-                <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
-            </li>
-            <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
-        </ul>
     </div>
     <!--end content-->
     </main>
